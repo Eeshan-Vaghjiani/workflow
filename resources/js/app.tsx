5819eeaf94e294f1eb0bfef20c3b4b1f11477206
dhb001 <dhruvinbhudiael11@gmail.com>
2025-05-05 14:17:42 +0300

    minor bug fix

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -9,10 +9,16 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    resolve: (name) => {
+        console.log('Resolving page:', name);
+        const pages = import.meta.glob('./pages/**/*.tsx', { eager: true });
+        console.log('Available pages:', Object.keys(pages));
+        const page = resolvePageComponent(`./pages/${name}.tsx`, pages);
+        console.log('Resolved page:', page);
+        return page;
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
-
         root.render(<App {...props} />);
     },
     progress: {

75a4dfbc49238f62e099b9b4aad1dc5e7e80d627
eeshan-vaghjiani <evaghjiani@gmail.com>
2025-05-09 22:59:49 +0300

    laravel pusher

--- config/app.php
+++ config/app.php
@@ -137,7 +137,7 @@ return [
         // App\Providers\AuthServiceProvider::class, // Commented out as it doesn't exist
         App\Providers\BroadcastServiceProvider::class,
         // App\Providers\EventServiceProvider::class, // Commented out as it doesn't exist
-        // App\Providers\RouteServiceProvider::class, // Commented out as it doesn't exist
+        App\Providers\RouteServiceProvider::class,
     ],
 
 ];
